0309747b811bbbafa6a4cf66baa76aedefaecb90
pafable <39135798+pafable@users.noreply.github.com>
2022-11-24 11:35:03 -0500

    Update update-container.sh

--- update-container.sh
+++ update-container.sh
@@ -5,19 +5,19 @@ DCKRCMP=$(which docker-compose)
 IMAGE="pihole/pihole:latest"
 
 function main() {
-  echo "Shutting down pihole container"
+  printf "Shutting down pihole container"
   ${DCKRCMP} down
 
-  echo "Pulling down latest pihole image"
+  printf "\nPulling down latest pihole image"
   ${DCKR} pull ${IMAGE}
 
-  echo "Starting new pihole container"
+  printf "\nStarting new pihole container"
   ${DCKRCMP} up -d
 
-  echo "Displaying new pihole container logs"
+  printf "\nDisplaying new pihole container logs"
   ${DCKRCMP} logs
 
-  echo "Deleting previous pihole image"
+  printf "\nDeleting previous pihole image"
   ${DCKR} image prune
 }
 
